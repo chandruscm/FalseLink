755f6b945ee40b4a07b1415b996598525568d02f
chandruscm <chandru.guitar@gmail.com>
2019-11-18 00:22:41 +0100

    Implement PoC dialog

--- prototype/app/src/main/java/com/chandruscm/whatthelink/ui/verify/VerifyViewModel.kt
+++ prototype/app/src/main/java/com/chandruscm/whatthelink/ui/verify/VerifyViewModel.kt
@@ -1,6 +1,9 @@
 package com.chandruscm.whatthelink.ui.verify
 
 import android.net.Uri
+import android.os.Handler
+import androidx.core.os.postDelayed
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
@@ -12,6 +15,16 @@ class VerifyViewModel @AssistedInject constructor(
     @AssistedInject.Factory
     interface Factory {
         fun create(uri: Uri?): VerifyViewModel
+    }
+
+    private val handler = Handler()
+
+    val verificationStatus = MutableLiveData(false)
+
+    fun verifyLink() {
+        handler.postDelayed(5000) {
+            verificationStatus.postValue(true)
+        }
     }
 
     fun getUri() = uri
